3dadad0a02e0e92966aae226fdc30f3f0cf9c4e6
Cristina González Castellano <cristina.gonzalez@liferay.com>
2015-08-31 10:08:01 +0200

    [arquillian/arquillian-extension-liferay#27] Add test for the injection of bundle

--- arquillian-extension-liferay-example/src/test/java/org/arquillian/liferay/test/TestInjections.java
+++ arquillian-extension-liferay-example/src/test/java/org/arquillian/liferay/test/TestInjections.java
@@ -14,35 +14,50 @@
 
 package org.arquillian.liferay.test;
 
+import com.liferay.portal.model.Release;
+import com.liferay.portal.service.ReleaseLocalService;
+
 import java.io.InputStream;
+
+import java.util.UUID;
+
+import org.arquillian.container.liferay.remote.enricher.Inject;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.osgi.metadata.OSGiManifestBuilder;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.Asset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
 
 /**
  * @author Cristina González
  */
 @RunWith(Arquillian.class)
-public class SimpleTest {
+public class TestInjections {
 
 	@Deployment
 	public static JavaArchive create() {
 		final JavaArchive archive = ShrinkWrap.create(
-			JavaArchive.class, "bundle.jar");
+			JavaArchive.class, "bundle" + UUID.randomUUID() + ".jar");
 
 		archive.setManifest(new Asset() {
 			@Override
 			public InputStream openStream() {
 				OSGiManifestBuilder builder = OSGiManifestBuilder.newInstance();
-				builder.addBundleSymbolicName(archive.getName());
+				builder.addBundleSymbolicName("bundle");
 				builder.addBundleManifestVersion(2);
+				builder.addImportPackages(
+					ReleaseLocalService.class.getPackage().getName());
+				builder.addImportPackages(Release.class.getPackage().getName());
 				return builder.openStream();
 			}
 		});
@@ -50,8 +65,33 @@ public class SimpleTest {
 	}
 
 	@Test
-	public void test() {
-		System.out.println("Example of test executed in Liferay");
+	public void shouldInjectBundle() {
+		Assert.assertNotNull(_bundle);
+		Assert.assertEquals("bundle", _bundle.getSymbolicName());
 	}
+
+	@Test
+	public void shouldInjectBundleCOntext() {
+		Assert.assertNotNull(_bundleContext);
+		Assert.assertEquals(_bundle, _bundleContext.getBundle());
+	}
+
+	@Test
+	public void shouldInjectReleaseLocalService() {
+		Assert.assertNotNull(_releaseLocalService);
+
+		Release releasePortal = _releaseLocalService.fetchRelease("portal");
+
+		Assert.assertEquals(7000, releasePortal.getBuildNumber());
+	}
+
+	@ArquillianResource
+	private Bundle _bundle;
+
+	@ArquillianResource
+	private BundleContext _bundleContext;
+
+	@Inject
+	private ReleaseLocalService _releaseLocalService;
 
 }
