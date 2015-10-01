f9ea5d6c03dbdc5f262c05011de18c848db494dc
cgoncas <cristina.gonzalez@liferay.com>
2015-10-01 23:07:43 +0200

    [arquillian/arquillian-extension-liferay#30] New test for a portlet service

---
+++ arquillian-extension-liferay-example/src/test/java/org/arquillian/liferay/test/BasicPortletIntegrationTest.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ * <p/>
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * <p/>
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package org.arquillian.liferay.test;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.arquillian.container.liferay.remote.enricher.Inject;
+import org.arquillian.liferay.sample.service.SampleService;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.osgi.api.BndProjectBuilder;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Cristina González
+ */
+@RunWith(Arquillian.class)
+public class BasicPortletIntegrationTest {
+
+	@Deployment
+	public static JavaArchive create() {
+		BndProjectBuilder bndProjectBuilder = ShrinkWrap.create(
+			BndProjectBuilder.class);
+
+		bndProjectBuilder.setBndFile(new File("bnd-basic-portlet-test.bnd"));
+
+		bndProjectBuilder.generateManifest(true);
+
+		return bndProjectBuilder.as(JavaArchive.class);
+	}
+
+	@Test
+	public void testAdd() throws IOException, PortalException {
+		int result = _sampleService.add(1, 3);
+
+		Assert.assertEquals(4, result);
+	}
+
+	@Inject
+	private SampleService _sampleService;
+
+}
