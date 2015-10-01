b73ff1b22ab760a3d3d118443cf6148d61a26e55
cgoncas <cristina.gonzalez@liferay.com>
2015-10-01 23:11:44 +0200

    [arquillian/arquillian-extension-liferay#30] New portlet for testing purpouse

---
+++ arquillian-extension-liferay-example/src/main/java/org/arquillian/liferay/sample/portlet/SamplePortlet.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ * <p/>
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * <p/>
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package org.arquillian.liferay.sample.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.portlet.GenericPortlet;
+import javax.portlet.Portlet;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+
+/**
+ * @author Cristina González
+ */
+@Component(
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=Arquillian Sample Portlet",
+		"javax.portlet.name=arquillian_sample_portlet",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class SamplePortlet extends GenericPortlet {
+
+	@Override
+	protected void doView(RenderRequest request, RenderResponse response)
+		throws IOException, PortletException {
+
+		PrintWriter printWriter = response.getWriter();
+
+		printWriter.print("Sample Portlet is working!");
+	}
+}
